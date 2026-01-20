8ef5013948044147f8d1c7f8e57ed7917837d43f
limidoradevelopments-dev <limidora.developments@gmail.com>
2026-01-20 20:51:36 +0530

    seww

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,7 +102,7 @@ export default function LoginPage() {
   return (
     <div className="relative min-h-screen w-full bg-background">
       <Image
-        src="https://images.unsplash.com/photo-1668962857273-6cab55849320?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Abstract background"
         fill
         className="object-cover"
